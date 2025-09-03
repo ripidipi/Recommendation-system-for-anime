dbe342a003010e46c397f914be83d2615c3c05d3
Ivan <ripidipin@gmail.com>
2025-09-04 00:09:59 +0300

    invalid version of fileout but with lite filtering

--- ParseThroughAPI/src/main/java/Scripts/DataOutputToFile.java
+++ ParseThroughAPI/src/main/java/Scripts/DataOutputToFile.java
@@ -10,18 +10,44 @@ import java.util.Set;
 
 public class DataOutputToFile {
 
-    private final int MINIMUM_NUMBER_OF_ANIME_IN_USER_LISTS;
-    private final int MINIMUM_NUMBER_OF_COMPLETED_ANIME_IN_USER_LISTS;
-    private final int QUANTITY_OF_USER_TO_EXTRACT;
+    private int MINIMUM_NUMBER_OF_ANIME_IN_USER_LISTS;
+    private int MINIMUM_NUMBER_OF_COMPLETED_ANIME_IN_USER_LISTS;
+    private int QUANTITY_OF_USER_TO_EXTRACT;
     private final SimpleDataExtract extractor;
 
     private static Set<String> USER_ANIME_COLUMNS = Set.of(
-            "user_id","anime_id","score","status"
+            "user_id", "anime_id", "score", "status"
     );
     private static Set<String> ANIME_COLUMNS = Set.of(
             "mal_id","episodes","score","scored_by", "rating", "season", "type",
             "title", "synopsis", "status"
     );
+    private static Set<String> ANIME_JOINS_COLUMNS = Set.of(
+            """
+                    (SELECT STRING_AGG(DISTINCT g.name, ', ')
+                         FROM genre g
+                         JOIN anime_genre ag ON g.mal_id = ag.genre_id
+                         WHERE ag.anime_id = a.mal_id) AS genres
+            """, """
+                    (SELECT STRING_AGG(DISTINCT g.name, ', ')
+                         FROM genre g
+                         JOIN anime_theme ag ON g.mal_id = ag.genre_id
+                         WHERE ag.anime_id = a.mal_id) AS themes
+            """, """
+                    (SELECT STRING_AGG(DISTINCT d.name, ', ')
+                         FROM demographic d
+                         JOIN anime_demographic ad ON d.mal_id = ad.demographic_id
+                         WHERE ad.anime_id = a.mal_id) AS demographics
+            """, """
+                    (SELECT STRING_AGG(DISTINCT s.name, ', ')
+                         FROM producer s
+                         JOIN anime_studio as_ ON s.mal_id = as_.producer_id
+                         WHERE as_.anime_id = a.mal_id) AS studios
+            """);
+    private static Set<String> ANIME_EVALUATION = Set.of();
+    private static Set<String> ANIME_EVALUATION_FILTERS = Set.of("us.total_entries > ?", "us.completed > ?");
+    private static Set<String> ANIME_FILTERS = Set.of();
+
 
     public static void setUserAnimeColumns(Set<String> userAnimeColumns) {
         USER_ANIME_COLUMNS = userAnimeColumns;
@@ -31,12 +57,12 @@ public class DataOutputToFile {
         ANIME_COLUMNS = animeColumns;
     }
 
-    public static Set<String> getUserAnimeColumns() {
-        return USER_ANIME_COLUMNS;
+    public static void setAnimeJoins(Set<String> animeJoins) {
+        ANIME_JOINS_COLUMNS = animeJoins;
     }
 
-    public static Set<String> getAnimeColumns() {
-        return ANIME_COLUMNS;
+    public static void setAnimeEvaluation(Set<String> animeEvaluation) {
+        ANIME_EVALUATION = animeEvaluation;
     }
 
     public DataOutputToFile(int minNumberOfAnimeInLists, int minNumberOfCompletedAnimeInLists,
@@ -59,29 +85,66 @@ public class DataOutputToFile {
     public void close() { extractor.close(); }
 
     private String buildSelect(String table, Set<String> columns) {
-        return "SELECT " + String.join(", ", columns) + " FROM " + table;
+        return "SELECT " + String.join(",\n", columns) + " FROM " + table;
     }
 
-    private Set<String> buildColumnsForTable(String table_variable, Set<String> columns) {
+    private String buildFilter(Set<String> filters) {
+        if (filters.isEmpty()) {
+            return "";
+        }
+        return "WHERE " + String.join(" AND ", filters);
+    }
+
+    private Set<String> buildColumnsForTable(String table_variable, Set<String> columns, Set<String> rowLines) {
         Set<String> newColumns = new HashSet<>();
         for (String column : columns) {
             newColumns.add(table_variable + "." + column);
         }
-        return columns;
+        newColumns.addAll(rowLines);
+        return newColumns;
     }
 
-    public void extractUsersToFile(File outDir) throws Exception {
+    public void extractUsersToFile(File outDir) {
         if (!outDir.exists()) outDir.mkdirs();
+        try {
+            File animeFile = new File(outDir, "anime.parquet");
+            String animeSql = buildSelect("anime a",
+                    buildColumnsForTable("a", ANIME_COLUMNS, ANIME_JOINS_COLUMNS)) +
+                    buildFilter(ANIME_FILTERS);
+            extractor.exportQueryToParquet(animeSql, null, animeFile, null);
 
-        File animeFile = new File(outDir, "anime.parquet");
-        String animeSql = buildSelect("anime", buildColumnsForTable("a", ANIME_COLUMNS));
-        extractor.exportQueryToParquet(animeSql, null, animeFile, null);
+            File usersFile = new File(outDir, "users.parquet");
+            String usersSql = buildSelect("user_anime_stat r",
+                    buildColumnsForTable("r", USER_ANIME_COLUMNS, ANIME_EVALUATION)) +
+                            """
+                            \nJOIN
+                                user_stat us ON us.user_id = r.user_id
+                            """ +
+                    buildFilter(ANIME_EVALUATION_FILTERS);
+            System.out.println(usersSql);
+            extractor.exportQueryToParquet(usersSql, List.of(MINIMUM_NUMBER_OF_ANIME_IN_USER_LISTS,
+                    MINIMUM_NUMBER_OF_COMPLETED_ANIME_IN_USER_LISTS), usersFile, Set.of("user_id"));
 
-        File usersFile = new File(outDir, "users.parquet");
-        String usersSql = buildSelect("users_anime_stat", buildColumnsForTable("r", USER_ANIME_COLUMNS));
-        extractor.exportQueryToParquet(usersSql, null, usersFile, Set.of("user_id"));
-
-        System.out.println("Export finished to " + outDir.getAbsolutePath());
+            System.out.println("Export finished to " + outDir.getAbsolutePath());
+        } catch ( Exception e ) {
+            System.out.println(e.getMessage());
+        }
     }
 
+
+    public static void main(String[] args) {
+        Properties dbProps = new Properties();
+        String dbUrl = System.getProperty("hibernate.hikari.dataSource.url",
+                System.getenv().getOrDefault("DB_URL", "jdbc:postgresql://localhost:5432/anime_db"));
+        String dbUser = System.getProperty("hibernate.hikari.dataSource.user",
+                System.getenv().getOrDefault("DB_USER", "rudeus"));
+        String dbPass = System.getProperty("hibernate.hikari.dataSource.password",
+                System.getenv().getOrDefault("DB_PASS", "sylphi_pants"));
+        dbProps.setProperty("jdbc.url", dbUrl);
+        dbProps.setProperty("jdbc.user", dbUser);
+        dbProps.setProperty("jdbc.password", dbPass);
+        DataOutputToFile outFile = new DataOutputToFile(dbProps);
+        File outDir = new File("out");
+        outFile.extractUsersToFile(outDir);
+    }
 }
